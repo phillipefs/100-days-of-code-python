35f1cb77112b86bb1d56149e8590a8c497cd9c7d
Phillipe Santos <phillipefs@msn.com>
2022-11-03 21:50:28 -0300

    Day 03

---
+++ Day03 - Control Flow and Logical Operators/interactive_coding_exercise_bmi_calculator2.py
@@ -0,0 +1,23 @@
+### BMI 2.0 ###
+
+height = float(input("enter your height in m: "))
+weight = float(input("enter your height in kg: "))
+
+bmi = int(weight / height ** 2)
+
+
+if bmi <= 18.5:
+    print(f"Your BMI is {bmi}, you are underweight")
+
+elif bmi < 25:
+    print(f"Your BMI is {bmi}, you are normal weight")
+
+elif bmi < 30:
+    print(f"Your BMI is {bmi}, you are overweight")
+
+elif bmi < 35:
+    print(f"Your BMI is {bmi}, you are obese")
+
+else:
+    print(f"Your BMI is {bmi}, you are clinically obese")
+
---
+++ Day03 - Control Flow and Logical Operators/interactive_coding_exercise_odd_even.py
@@ -0,0 +1,10 @@
+### ODD OR EVEN ###
+
+number = int(input("Digite one number: "))
+
+mod = number % 2
+
+if mod > 0 :
+    print(f"Number {number} is ODD.")
+else:
+    print(f"Number {number} is EVEN.")
